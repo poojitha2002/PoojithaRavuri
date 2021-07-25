7077e741f8dc4124b86e25cd6232ed33cb6d303d
Ravuri Poojitha <ravuri.poojitha123@gmail.com>
2021-07-25 13:27:03 +0530

    final

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,6 +18,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from django.views.static import serve
+
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
